d811e107cdd4f0d9e0243d6857b11b1aa7709069
neilpeterson <neilpeterson@outlook.com>
2019-11-12 05:30:45 -0800

    Test

--- ops40/demos/azure_pipeline/tests/pester.tests.ps1
+++ ops40/demos/azure_pipeline/tests/pester.tests.ps1
@@ -1,16 +1,14 @@
 param (
+    [string]$valuesFile="values.yaml",
     [string]$hostName
 )
 
 $valuesFile='values.yaml'
-$hostObjct = Get-Content $valuesFile
-$host = $hostObjct[37].split(" ")[7]
 
 Describe 'Cart Host' {
-  $hostObjct = Get-Content $valuesFile
-  $host = $hostObjct[37].split(" ")[7]
+    $hostObject = Get-Content $valuesFile
 
     It 'A test that should be true' {
-      $host  | Should -Be $hostName
+      $hostObject[37].split(" ")[7] | Should -Be $hostName
     }
   }
